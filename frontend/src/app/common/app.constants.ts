8ad903c9a80885ca0b710a1740df4c8137e0e8ed
Paweł Makowski <m4kpaul@gmail.com>
2021-07-06 01:40:13 +0200

    Update url configs

--- frontend/src/app/common/app.constants.ts
+++ frontend/src/app/common/app.constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
-  private static API_BASE_URL = 'http://localhost:8080/';
-  private static REDIRECT_URL = '?redirect_uri=http://localhost:8081/login';
+  private static API_BASE_URL = 'http://127.0.0.1:8080/'; // update
+  private static REDIRECT_URL = '?redirect_uri=http://127.0.0.1:8081/login';
 
   public static API_URL = AppConstants.API_BASE_URL + 'api/';
   public static AUTH_API = AppConstants.API_URL + 'auth/';
